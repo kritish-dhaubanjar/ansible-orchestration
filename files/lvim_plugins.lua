05c8339efc4e08763a1c5771c137e0ef5a5f6489
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2024-11-04 00:02:42 +0545

    Update lvim_plugins.lua

--- files/lvim_plugins.lua
+++ files/lvim_plugins.lua
@@ -1,26 +1,41 @@
--- add your own keymapping
+vim.opt.mouse = ""
+vim.opt.list = true
+vim.opt.wrap = true
+vim.opt.updatetime = 50;
+vim.opt.relativenumber = true;
+vim.opt.fillchars = {
+  vert = "▕", -- alternatives │
+  fold = " ",
+  eob = " ", -- suppress ~ at EndOfBuffer
+  diff = "╱", -- alternatives = ⣿ ░ ─
+  msgsep = "‾",
+  foldopen = "▾",
+  foldsep = "│",
+  foldclose = "▸",
+}
+
 lvim.keys.normal_mode["<M-l>"] = ":BufferLineMoveNext<CR>"
 lvim.keys.normal_mode["<M-h>"] = ":BufferLineMovePrev<CR>"
 
+-- lvim.colorscheme = "catppuccin" -- catppuccin catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
+lvim.colorscheme = "catppuccin-mocha" -- catppuccin catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
+lvim.transparent_window = false
+lvim.builtin.terminal.open_mapping = "<c-t>"
+
+-- use treesitter folding
+-- vim.opt.foldmethod = "expr"
+-- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
 lvim.plugins = {
-  -- trouble.nvim
-  {
-    "folke/trouble.nvim",
-    cmd = "TroubleToggle",
-  },
-  -- git-blame
-  {
-    "f-person/git-blame.nvim",
-    event = "BufRead",
-    config = function()
-      vim.cmd "highlight default link gitblame SpecialComment"
-      vim.g.gitblame_enabled = 0
-    end,
-  },
   -- diffview
   {
     "sindrets/diffview.nvim",
     event = "BufRead",
+  },
+  -- trouble.nvim
+  {
+    "folke/trouble.nvim",
+    cmd = "TroubleToggle",
   },
   -- vim-surround
   {
@@ -31,29 +46,29 @@ lvim.plugins = {
     --  vim.o.timeoutlen = 500
     -- end
   },
+  -- vim-sanegx
   {
-    "nvim-orgmode/orgmode",
-    require('orgmode').setup_ts_grammar(),
-
-    -- Treesitter configuration
-    require('nvim-treesitter.configs').setup {
-      -- If TS highlights are not enabled at all, or disabled via `disable` prop,
-      -- highlighting will fallback to default Vim syntax highlighting
-      highlight = {
-        enable = true,
-        -- Required for spellcheck, some LaTex highlights and
-        -- code block highlights that do not have ts grammar
-        additional_vim_regex_highlighting = { 'org' },
-      },
-      ensure_installed = { 'org' }, -- Or run :TSUpdate org
-    },
-
-    require('orgmode').setup({
-      org_agenda_files = { '~/Dropbox/org/*', '~/my-orgs/**/*' },
-      org_default_notes_file = '~/Dropbox/org/refile.org',
-    })
+    "felipec/vim-sanegx",
+    event = "BufRead",
   },
-  -- mini.map
+  -- todo-comments.nvim
+  {
+    "folke/todo-comments.nvim",
+    event = "BufRead",
+    config = function()
+      require("todo-comments").setup()
+    end,
+  },
+  -- git-blame
+  {
+    "f-person/git-blame.nvim",
+    event = "BufRead",
+    config = function()
+      vim.cmd "highlight default link gitblame SpecialComment"
+      vim.g.gitblame_enabled = 0
+    end,
+  },
+  --- mini.map
   {
     "echasnovski/mini.map",
     branch = "stable",
@@ -61,33 +76,50 @@ lvim.plugins = {
       require('mini.map').setup()
       local map = require('mini.map')
       map.setup({
-        integrations = {
-          map.gen_integration.builtin_search(),
-          map.gen_integration.diagnostic({
-            error = 'DiagnosticFloatingError',
-            warn  = 'DiagnosticFloatingWarn',
-            info  = 'DiagnosticFloatingInfo',
-            hint  = 'DiagnosticFloatingHint',
-          }),
-        },
         symbols = {
           encode = map.gen_encode_symbols.dot('4x2'),
-        },
-        window = {
-          side = 'right',
-          width = 20, -- set to 1 for a pure scrollbar :)
-          winblend = 15,
-          show_integration_count = false,
-        },
+        }
       })
     end
+  },
+  -- nvim-ts-autotag
+  {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  -- orgmode
+  {
+    'nvim-orgmode/orgmode',
+    dependencies = {
+      { 'nvim-treesitter/nvim-treesitter', lazy = true },
+    },
+    event = 'VeryLazy',
+    config = function()
+      -- Setup treesitter
+      require('nvim-treesitter.configs').setup({
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = { 'org' },
+        },
+        ensure_installed = { 'org' },
+      })
+
+      -- Setup orgmode
+      require('orgmode').setup({
+        org_agenda_files = '~/kritish-dhaubanjar/org-archives/**/*',
+        org_default_notes_file = '~/kritish-dhaubanjar/org-archives/refile.org',
+      })
+    end,
   }
 }
 
 -- mini.map
 lvim.autocommands = {
   {
-    {"BufEnter", "Filetype"},
+    { "BufEnter", "Filetype" },
     {
       desc = "Open mini.map and exclude some filetypes",
       pattern = { "*" },
@@ -113,36 +145,13 @@ lvim.autocommands = {
   },
 }
 
-vim.opt.mouse = ""
-vim.opt.list = true
-vim.opt.updatetime = 50
-vim.opt.relativenumber = true
-
-vim.opt.fillchars = {
-  vert = "▕", -- alternatives │
-  fold = " ",
-  eob = " ", -- suppress ~ at EndOfBuffer
-  diff = "╱", -- alternatives = ⣿ ░ ─
-  msgsep = "‾",
-  foldopen = "▾",
-  foldsep = "│",
-  foldclose = "▸",
-}
-
-lvim.transparent_window = true
-lvim.builtin.terminal.open_mapping = "<c-t>"
-
+-- telescope
 lvim.builtin.telescope = {
   active = true,
   defaults = {
     layout_strategy = "horizontal",
-  },
-  pickers = {
-    git_files = {
-      hidden = true,
-    },
-    live_grep = {
-      hidden = true,
-    },
-  },
+    layout_config = {
+      width = 0.7,
+    }
+  }
 }
